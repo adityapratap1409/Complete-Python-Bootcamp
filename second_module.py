9847d9871995f2ebc6dba26eaa7d37182caeef13
adityapratap1409 <aditya.sjc@gmail.com>
2026-08-23 01:32:29 +0530

    Add practice.py and second_module.py with initial code examples

---
+++ second_module.py
@@ -0,0 +1,18 @@
+a=2
+b=7
+print(a+b)
+print(2+3-4-8+421-23526)
+print(a)
+print(b)
+b=2.1
+print(a)
+print(b)
+print(a+b)
+print(2.1+213.123-213.21)
+a=2+3j
+b=a+17j
+print(a)
+print(b)
+print(a+b)
+print("String with double quotes")
+print('String with single quotes')
