d2849146c6bcd497e29da64be8d38c398fd0fe0f
adityapratap1409 <aditya.sjc@gmail.com>
2026-08-23 17:20:18 +0530

    Refactor second_module.py for concise code and add bmi_calc.py for BMI calculation

---
+++ bmi_calc.py
@@ -0,0 +1,7 @@
+#print("Choose your unit system- ")
+#if()
+height=int(input('Enter height- '))
+weight=int(input("Enter weight- "))
+bmi=(weight/(height*height))*10000
+print("Your bmi is- ",bmi)
+
--- second_module.py
+++ second_module.py
@@ -1,18 +1,26 @@
-a=2
-b=7
-print(a+b)
-print(2+3-4-8+421-23526)
-print(a)
-print(b)
-b=2.1
+#Number addition and other things related to basic syntax
+a=2;b=7;print(a+b);print(2+3-4-8+421-23526);print(a);print(b);b=2.1;print(a);print(b);print(a+b);print(2.1+213.123-213.21)
+a=2+3j;b=a+17j
 print(a)
 print(b)
 print(a+b)
-print(2.1+213.123-213.21)
-a=2+3j
-b=a+17j
-print(a)
-print(b)
-print(a+b)
+#Strings and its basic syntax in python
 print("String with double quotes")
 print('String with single quotes')
+print('Python'+'.py')#Concatenation of string
+#Boolean
+print(1231>123)
+print(12313<645689)
+print(1==93)
+print(1==1)
+#Swapping methods
+a=10
+b=19
+print(a,' ',b)
+temp=a
+a=b
+b=temp
+print(a,' ',b)
+#One liner method
+a,b=b,a
+print(a,' ',b)
