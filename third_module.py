a24dbfd72662f31de4192c9c24a531d23f6bddbd
adityapratap1409 <aditya.sjc@gmail.com>
2026-08-27 23:38:42 +0530

    Update settings.json to use virtual environment and add third_module.py with bitwise operations examples

---
+++ third_module.py
@@ -0,0 +1,3 @@
+str='python memebership operator';print('python'in str);print('in' not in str)
+A=10;B=4;print('Value of Bitwise AND', A&B);print('Value of Bitwise OR', A|B);print('Value of Bitwise XOR', A^B)
+print('Value of Bitwise NOT', ~A);print('Value of Left Shift Bitwise', A<<1);print('Value of Right Shift Bitwise', A>>1)
