007c59d94d2a8e761746400869c8b043fc59b9db
Aditya Pratap Singh <aditya.sjc@gmail.com>
2026-09-02 13:38:02 +0530

    Remove class_notes.py, update second_module.py for consistency, and add 31-08.py with membership operator examples

--- 31-08.py
+++ 31-08.py
@@ -5,7 +5,13 @@ else:
 for i in range(1,4):
     for j in range(4,7):
         print(i*j)'''
+#in is a membership operator
 n=eval(input("enter a "))
 for i in range(1,n,1):
-    if(i%5==0 and i%10!=0):
-        print(i)
+    if(i%5==0):
+        if(i%10==0):
+            pass
+        else:
+         print(i)
+x = 'Engineering';print('g' in x and 'z' not in x)
+
--- third_module.py
+++ third_module.py
@@ -3,3 +3,12 @@ A=10;B=4;print('Value of Bitwise AND', A&B);print('Value of Bitwise OR', A|B);pr
 print('Value of Bitwise NOT', ~A);print('Value of Left Shift Bitwise', A<<1);print('Value of Right Shift Bitwise', A>>1)
 # Type function identifying data type
 a=10;b=2+3j;c=10.0003;d='String';print(type(a));print(type(b));print(type(c));print(type(d))
+if(1=='1'):
+ print('false')
+else:
+ print('true')
+x=[1,2,3,4,5]
+print(x is x)
+print(x is [1,2,3,4,5])
+print(x is not x)
+print(x is not [1,2,3,4,5])
